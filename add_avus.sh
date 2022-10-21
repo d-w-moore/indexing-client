2f2329e48490d431fdc8fe9e13dfa7e5146ff637
d-w-moore <dmoore@renci.org>
2022-10-21 03:57:02 +0000

    first commit

---
+++ add_avus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DELIM=";;;"
+while read LINE; do
+    Y=($(sed "s/$DELIM/\n/g"<<<"$LINE"))
+    if [ ${Y[4]} = "-1" ]; then
+        t=COLL
+    else
+        t=DATA
+    fi
+    AVUs=$(iquest --no-page ';;;%s;;;%s;;;%s' "select order(META_${t}_ATTR_NAME), META_${t}_ATTR_VALUE, META_${t}_ATTR_UNITS where ${t}_ID = '${Y[0]}'")
+    if [[ $AVUs =~ 'CAT_NO_ROWS' ]]; then AVUs="" 
+    else
+        AVUs=$(tr -d "\n" <<<"$AVUs")
+    fi
+    echo "$LINE$AVUs"
+done
---
+++ as_json.py
@@ -0,0 +1,19 @@
+import sys,pprint
+for x in sys.stdin:
+    y = x.split(";;;")
+    ID,MTIME,NAME,PARENT,SIZE = y[:5]
+    SIZE=int(SIZE)
+    data_size,is_file = max(SIZE,0),(SIZE >= 0)
+    logical_path = "{PARENT}/{NAME}".format(**locals()) if is_file else NAME
+    record = {"metadataEntries":list(
+                  dict(zip(("attribute","value","unit"),y[4+j*3:7+j*3])) for j in
+               range((len(y)-5)//3)
+               )
+             ,"dataSize": data_size
+             ,"fileName": NAME
+             ,"zoneName": logical_path.split("/",1)[0]
+             ,"absolutePath": logical_path
+             ,"lastModifiedDate": int(MTIME,10)
+             ,"isFile":is_file
+    }
+    pprint.pprint (record)
