d7487257620c5a1a67b03af47464f46da3ec638a
d-w-moore <dmoore@renci.org>
2022-10-22 13:53:17 +0000

    output curl commands by default

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 ROOT_COLL="/tempZone/home/rods/v1"
 { bash get_objects.sh c "$ROOT_COLL"; bash get_objects.sh d "$ROOT_COLL" ; } |tee obj
 python3 add_avus.py < obj |tee obj2
-python3 as_json.py < obj2 > json.out
+Then:
+   * python3 as_json.py < obj2 > json.out
+   or:
+   * python
 ```
--- as_json.py
+++ as_json.py
@@ -1,9 +1,34 @@
-import sys,pprint,json
+import base64
+import json
+import pprint
+import sys
+import getopt
+
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
+
 remove_cr = lambda List:List[:-1] + [elem.rstrip("\n") for elem in List[-1:]]
+
+opt,arg = getopt.getopt(sys.argv[1:],'qvf:')
+optD = dict(opt)
+
+output_format = optD.get('-f')
+
+if not output_format:
+    output_format = '''curl -H "Content-Type: application/json" -X POST http://localhost:9200/metadata_idx/_doc/{ID} -d {SHELL_QUOTED_JSON}'''
+
+if '-v' in optD:
+    print ('output_format =',output_format, file = sys.stderr)
+
+if '-q' in optD:
+    exit()
+
 for x in sys.stdin:
     y = x.split(";;;")
     ID,MTIME,NAME,PARENT,SIZE = y[:5]
-    SIZE=int(SIZE)
+    SIZE = int(SIZE)
     data_size,is_file = max(SIZE,0),(SIZE >= 0)
     logical_path = "{PARENT}/{NAME}".format(**locals()) if is_file else NAME
     record = {"metadataEntries":list(
@@ -17,4 +42,6 @@ for x in sys.stdin:
              ,"lastModifiedDate": int(MTIME,10)
              ,"isFile":is_file
     }
-    print(json.dumps(record,separators=(',',':')).rstrip("\n"))
+    JSON = json.dumps(record,separators=(',',':')).rstrip("\n")
+    SHELL_QUOTED_JSON = quote(JSON)
+    print(output_format.format(**locals()))
